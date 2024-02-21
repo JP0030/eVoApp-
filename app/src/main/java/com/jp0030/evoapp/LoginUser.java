636c3163a646b0ef8f5329cbc213db964ec9bda9
JP0030 <thekeen003@gmail.com>
2024-02-21 20:30:00 +0530

    in LogineUser ux is adde and on progress ...

--- app/src/main/java/com/jp0030/evoapp/LoginUser.java
+++ app/src/main/java/com/jp0030/evoapp/LoginUser.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class LoginUser extends AppCompatActivity {
     EditText edtUserName , edtEmail;
     Button btnStart;
+    ProgressBar progressBar3;
+    String phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +22,25 @@ public class LoginUser extends AppCompatActivity {
         edtUserName = findViewById(R.id.edtUserName);
         edtEmail = findViewById(R.id.edtEmail);
         btnStart = findViewById(R.id.btnStart);
+        progressBar3 = findViewById(R.id.progressBar3);
 
-        Intent homeActivity ;
-        homeActivity = new Intent(LoginUser.this, HomeActivity.class);
+        phoneNumber = getIntent().getExtras().getString("phone");
+        getUserName();
+        
+    }
 
-        btnStart.setOnClickListener(v -> startActivity(homeActivity));
+    void getUserName (){
+        setInProgress(true);
 
+    }
+
+    void setInProgress(boolean inProgress){
+        if (inProgress){
+            progressBar3.setVisibility(View.VISIBLE);
+            btnStart.setVisibility(View.GONE);
+        }else {
+            progressBar3.setVisibility(View.GONE);
+            btnStart.setVisibility(View.VISIBLE);
+        }
     }
 }
