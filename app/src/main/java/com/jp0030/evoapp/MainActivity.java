12911bce41a24714e3ac066aa4d51c2ec0788769
JP0030 <thekeen003@gmail.com>
2024-02-24 23:24:51 +0530

    ux added inmain activity

--- app/src/main/java/com/jp0030/evoapp/MainActivity.java
+++ app/src/main/java/com/jp0030/evoapp/MainActivity.java
@@ -1,14 +1,43 @@
 package com.jp0030.evoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ImageButton wallet;
+
+    ProfileFragment profileFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        wallet = findViewById(R.id.wallet);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.menuProfile){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame,profileFragment).commit();
+                }
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.menuProfile);
     }
 }
